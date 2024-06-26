3be90a4e6e9f7d4b3173e402f8de9ef4e89be6b1
Victor Galvez <vhgalvez@gmail.com>
2024-06-26 14:36:34 +0200

    update

--- br0_network/main.tf
+++ br0_network/main.tf
@@ -13,13 +13,6 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# Crear el pool 'default'
-resource "libvirt_pool" "default" {
-  name = "default"
-  type = "dir"
-  path = "/var/lib/libvirt/images"
-}
-
 resource "libvirt_network" "br0" {
   name      = var.rocky9_network_name
   mode      = "bridge"
