08792b93a52da2f671ba52aec1aa06f0896e6b1c
Victor Galvez <vhgalvez@gmail.com>
2024-07-11 19:24:53 +0200

    update

--- nat_network_03/main.tf
+++ nat_network_03/main.tf
@@ -78,7 +78,7 @@ resource "libvirt_volume" "vm_disk" {
   base_volume_id = libvirt_volume.base.id
   pool           = libvirt_pool.volumetmp_03.name
   format         = "qcow2"
-  size           = each.value.disk_size * 1024
+  size           = each.value.disk_size * 1024  # Convertir GB a MB
 }
 
 resource "libvirt_domain" "machine" {
