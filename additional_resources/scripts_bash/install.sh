4f1905d558e62a39610f1858fa25e4d9bf3a2e79
Victor Galvez <vhgalvez@gmail.com>
2024-09-09 19:06:21 +0200

    update

--- additional_resources/scripts_bash/install.sh
+++ additional_resources/scripts_bash/install.sh
@@ -1,8 +1,9 @@
 #/usr/bin/env sh
 set -ex
 
+# Variables de entorno y rutas
 DESTDIR=${DESTDIR:-}
-PREFIX=${PREFIX:-/usr/local}
+PREFIX=${PREFIX:-/opt}
 ETCDIR=${ETCDIR:-/etc}
 LIBEXECDIR=${LIBEXECDIR:-/opt/libexec}
 LIBEXEC_CRIO_DIR=${LIBEXEC_CRIO_DIR:-$LIBEXECDIR/crio}
@@ -18,7 +19,7 @@ FISHINSTALLDIR=${FISHINSTALLDIR:-$PREFIX/share/fish/completions}
 ZSHINSTALLDIR=${ZSHINSTALLDIR:-$PREFIX/share/zsh/site-functions}
 OPT_CNI_BIN_DIR=${OPT_CNI_BIN_DIR:-/opt/cni/bin}
 
-# Adjust SYSCONFIGDIR if we're running on debian based distributions
+# Ajustar SYSCONFIGDIR para distribuciones basadas en Debian
 if dpkg -l >/dev/null 2>&1; then
     SYSCONFIGDIR=${SYSCONFIGDIR:-$ETCDIR/default}
     sed -i "s;sysconfig/crio;default/crio;g" etc/crio
@@ -26,7 +27,7 @@ else
     SYSCONFIGDIR=${SYSCONFIGDIR:-$ETCDIR/sysconfig}
 fi
 
-# Update systemddir based on OS
+# Actualizar systemddir según el sistema operativo
 source /etc/os-release
 if { [[ "${ID}" == "fedora" ]] && [[ "${VARIANT_ID}" == "coreos" ]]; } ||
     [[ "${ID}" == "rhcos" ]]; then
@@ -41,40 +42,42 @@ if selinuxenabled 2>/dev/null; then
 fi
 ARCH=${ARCH:-amd64}
 
+# Instalación de directorios y archivos necesarios
 install $SELINUX -d -m 755 "$DESTDIR$CNIDIR"
-install $SELINUX -D -m 755 -t "$DESTDIR$OPT_CNI_BIN_DIR" /opt/crio/cri-o/cni-plugins/*
-install $SELINUX -D -m 644 -t "$DESTDIR$CNIDIR" contrib/11-crio-ipv4-bridge.conflist
+install $SELINUX -D -m 755 -t "$DESTDIR$OPT_CNI_BIN_DIR" /opt/bin/cri-o/cni-plugins/*
+install $SELINUX -D -m 644 -t "$DESTDIR$CNIDIR" /opt/bin/cri-o/contrib/11-crio-ipv4-bridge.conflist
 install $SELINUX -d -m 755 "$DESTDIR$LIBEXEC_CRIO_DIR"
-install $SELINUX -D -m 755 -t "$DESTDIR$LIBEXEC_CRIO_DIR" bin/conmon
-install $SELINUX -D -m 755 -t "$DESTDIR$LIBEXEC_CRIO_DIR" bin/conmonrs
-install $SELINUX -D -m 755 -t "$DESTDIR$LIBEXEC_CRIO_DIR" bin/crun
-install $SELINUX -D -m 755 -t "$DESTDIR$LIBEXEC_CRIO_DIR" bin/runc
+install $SELINUX -D -m 755 -t "$DESTDIR$LIBEXEC_CRIO_DIR" /opt/bin/cri-o/bin/conmon
+install $SELINUX -D -m 755 -t "$DESTDIR$LIBEXEC_CRIO_DIR" /opt/bin/cri-o/bin/conmonrs
+install $SELINUX -D -m 755 -t "$DESTDIR$LIBEXEC_CRIO_DIR" /opt/bin/cri-o/bin/crun
+install $SELINUX -D -m 755 -t "$DESTDIR$LIBEXEC_CRIO_DIR" /opt/bin/cri-o/bin/runc
 install $SELINUX -d -m 755 "$DESTDIR$BASHINSTALLDIR"
 install $SELINUX -d -m 755 "$DESTDIR$FISHINSTALLDIR"
 install $SELINUX -d -m 755 "$DESTDIR$ZSHINSTALLDIR"
 install $SELINUX -d -m 755 "$DESTDIR$CONTAINERS_REGISTRIES_CONFD_DIR"
-install $SELINUX -D -m 755 -t "$DESTDIR$BINDIR" bin/crio
-install $SELINUX -D -m 755 -t "$DESTDIR$BINDIR" bin/pinns
-install $SELINUX -D -m 755 -t "$DESTDIR$BINDIR" bin/crictl
-install $SELINUX -D -m 644 -t "$DESTDIR$ETCDIR" etc/crictl.yaml
-install $SELINUX -D -m 644 -t "$DESTDIR$OCIDIR" etc/crio-umount.conf
-install $SELINUX -D -m 644 -t "$DESTDIR$SYSCONFIGDIR" etc/crio
-install $SELINUX -D -m 644 -t "$DESTDIR$ETC_CRIO_DIR" contrib/policy.json
-install $SELINUX -D -m 644 -t "$DESTDIR$ETC_CRIO_DIR/crio.conf.d" etc/10-crio.conf
-install $SELINUX -D -m 644 -t "$DESTDIR$MANDIR/man5" man/crio.conf.5
-install $SELINUX -D -m 644 -t "$DESTDIR$MANDIR/man5" man/crio.conf.d.5
-install $SELINUX -D -m 644 -t "$DESTDIR$MANDIR/man8" man/crio.8
-install $SELINUX -D -m 644 -t "$DESTDIR$BASHINSTALLDIR" completions/bash/crio
-install $SELINUX -D -m 644 -t "$DESTDIR$FISHINSTALLDIR" completions/fish/crio.fish
-install $SELINUX -D -m 644 -t "$DESTDIR$ZSHINSTALLDIR" completions/zsh/_crio
-install $SELINUX -D -m 644 -t "$DESTDIR$SYSTEMDDIR" contrib/crio.service
-install $SELINUX -D -m 644 -t "$DESTDIR$CONTAINERS_REGISTRIES_CONFD_DIR" contrib/registries.conf
+install $SELINUX -D -m 755 -t "$DESTDIR$BINDIR" /opt/bin/cri-o/bin/crio
+install $SELINUX -D -m 755 -t "$DESTDIR$BINDIR" /opt/bin/cri-o/bin/pinns
+install $SELINUX -D -m 755 -t "$DESTDIR$BINDIR" /opt/bin/cri-o/bin/crictl
+install $SELINUX -D -m 644 -t "$DESTDIR$ETCDIR" /opt/bin/cri-o/etc/crictl.yaml
+install $SELINUX -D -m 644 -t "$DESTDIR$OCIDIR" /opt/bin/cri-o/etc/crio-umount.conf
+install $SELINUX -D -m 644 -t "$DESTDIR$SYSCONFIGDIR" /opt/bin/cri-o/etc/crio
+install $SELINUX -D -m 644 -t "$DESTDIR$ETC_CRIO_DIR" /opt/bin/cri-o/contrib/policy.json
+install $SELINUX -D -m 644 -t "$DESTDIR$ETC_CRIO_DIR/crio.conf.d" /opt/bin/cri-o/etc/10-crio.conf
+install $SELINUX -D -m 644 -t "$DESTDIR$MANDIR/man5" /opt/bin/cri-o/man/crio.conf.5
+install $SELINUX -D -m 644 -t "$DESTDIR$MANDIR/man5" /opt/bin/cri-o/man/crio.conf.d.5
+install $SELINUX -D -m 644 -t "$DESTDIR$MANDIR/man8" /opt/bin/cri-o/man/crio.8
+install $SELINUX -D -m 644 -t "$DESTDIR$BASHINSTALLDIR" /opt/bin/cri-o/completions/bash/crio
+install $SELINUX -D -m 644 -t "$DESTDIR$FISHINSTALLDIR" /opt/bin/cri-o/completions/fish/crio.fish
+install $SELINUX -D -m 644 -t "$DESTDIR$ZSHINSTALLDIR" /opt/bin/cri-o/completions/zsh/_crio
+install $SELINUX -D -m 644 -t "$DESTDIR$SYSTEMDDIR" /opt/bin/cri-o/contrib/crio.service
+install $SELINUX -D -m 644 -t "$DESTDIR$CONTAINERS_REGISTRIES_CONFD_DIR" /opt/bin/cri-o/contrib/registries.conf
 
-# Update the DESTDIR in the CRI-O configuration
+# Actualizar las rutas en la configuración de CRI-O
 sed -i 's;/usr/bin;'"$DESTDIR$BINDIR"';g' "$DESTDIR$ETC_CRIO_DIR/crio.conf.d/10-crio.conf"
-sed -i 's;/opt/libexec;'"$DESTDIR$LIBEXECDIR"';g' "$DESTDIR$ETC_CRIO_DIR/crio.conf.d/10-crio.conf"
+sed -i 's;/usr/libexec;'"$DESTDIR$LIBEXECDIR"';g' "$DESTDIR$ETC_CRIO_DIR/crio.conf.d/10-crio.conf"
 sed -i 's;/etc/crio;'"$DESTDIR$ETC_CRIO_DIR"';g' "$DESTDIR$ETC_CRIO_DIR/crio.conf.d/10-crio.conf"
 
+# Aplicar etiquetas SELinux si está habilitado
 if [ -n "$SELINUX" ]; then
     if command -v chcon >/dev/null; then
         chcon -u system_u -r object_r -t container_runtime_exec_t \
--- additional_resources/scripts_bash/install_crio.sh
+++ additional_resources/scripts_bash/install_crio.sh
@@ -4,6 +4,9 @@
 sudo mkdir -p /opt/bin /etc/kubernetes/pki /opt/cni/bin /etc/kubernetes/pki/etcd
 sudo mkdir -p /opt/cni/bin
 sudo mkdir -p  /home/core/bin/mksquashfs
+sudo mkdir -p /home/core/bin
+
+export PATH=$PATH:/home/core/bin
 
 
 # Descargar e instalar CRI-O (v1.30.5)
@@ -32,8 +35,15 @@ sudo docker ps
 sudo chmod +x /usr/local/bin/mksquashfs
 
 
+
+
 sudo docker cp <container_id>:/usr/bin/mksquashfs /home/core/bin/mksquashfs
 sudo chmod +x /home/core/bin/mksquashfs
+
+
+sudo docker cp ad2c56e6dfe9:/usr/bin/mksquashfs /home/core/bin/mksquashfs
+sudo chmod +x /home/core/bin/mksquashfs
+
 
 
 sudo mkdir -p /home/core/bin
@@ -87,8 +97,65 @@ export LD_LIBRARY_PATH=/home/core/lib:$LD_LIBRARY_PATH
 
 
 
+sudo docker exec -it <container_id> /bin/sh
+apk add lz4-libs lzo zstd-libs xz-libs
+sudo docker cp <container_id>:/usr/lib/liblz4.so.1 /home/core/lib/
+sudo docker cp <container_id>:/usr/lib/liblzo2.so.2 /home/core/lib/
+sudo docker cp <container_id>:/usr/lib/libzstd.so.1 /home/core/lib/
+sudo docker cp <container_id>:/usr/lib/libz.so.1 /home/core/lib/
+sudo docker cp <container_id>:/usr/lib/liblzma.so.5 /home/core/lib/
+
+
+
+export LD_LIBRARY_PATH=/home/core/lib:$LD_LIBRARY_PATH
+/home/core/bin/mksquashfs --version
 
 
 
 
 
+
+# Verifica el estado de la partición raíz
+mount | grep " / "
+
+# Verifica el estado de SELinux
+getenforce
+
+# Cambia temporalmente SELinux a modo permisivo
+sudo setenforce 0
+
+# Remonta la partición raíz con permisos de escritura
+sudo mount -o remount,rw /
+
+# Verifica errores en el sistema de archivos
+sudo dmesg | grep "EXT4-fs error"
+sudo fsck /dev/vda9  # Reemplaza con la partición correcta si es diferente
+
+# Si fsck no puede ejecutarse, crea un archivo para forzar el chequeo al reiniciar
+sudo touch /forcefsck
+
+# Reinicia el sistema para ejecutar fsck automáticamente
+sudo reboot
+
+# Intenta extraer archivos en un directorio diferente si /usr/local/bin/ está en modo solo lectura
+sudo mkdir -p /home/core/crio/
+sudo tar -xzf /tmp/crio.tar.gz -C /home/core/crio/
+
+
+
+
+
+core@bootstrap ~ $ sudo mount | grep " / "
+/dev/vda9 on / type ext4 (rw,relatime,seclabel)
+core@bootstrap ~ $ sudo mount -o remount,rw /
+core@bootstrap ~ $ mount | grep " / "
+/dev/vda9 on / type ext4 (rw,relatime,seclabel)
+core@bootstrap ~ $ sudo dmesg | grep "EXT4-fs error"
+core@bootstrap ~ $ sudo touch /forcefsck
+core@bootstrap ~ $
+sudo reboot
+
+https://www.flatcar.org/  https://okd.io/ https://www.redhat.com/es
+https://docs.okd.io/3.11/crio/crio_runtime.html
+https://github.com/okd-project/okd https://cri-o.io/
+https://kubernetes.io/
