84bc13eb88edc621ca71e7c877e173758a823f32
Victor Galvez <vhgalvez@gmail.com>
2024-09-26 14:29:17 +0200

    Update main.tf

--- nat_network_03/main.tf
+++ nat_network_03/main.tf
@@ -34,13 +34,20 @@ resource "libvirt_pool" "volume_pool" {
   depends_on = [null_resource.create_pool_directory]
 }
 
-# Add a delay to allow for pool initialization
-resource "null_resource" "wait_for_pool" {
+# Explicitly start the pool and autostart it
+resource "null_resource" "start_and_autostart_pool" {
   provisioner "local-exec" {
-    command = "sleep 5"
+    command = "sudo virsh pool-start volumetmp_03 && sudo virsh pool-autostart volumetmp_03"
   }
-
   depends_on = [libvirt_pool.volume_pool]
+}
+
+# Add a delay to allow for pool initialization and availability in libvirt
+resource "null_resource" "wait_for_pool_initialization" {
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+  depends_on = [null_resource.start_and_autostart_pool]
 }
 
 # Network Configuration for VMs
@@ -58,7 +65,7 @@ resource "libvirt_volume" "fcos_base" {
   source = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/34.20210626.3.0/x86_64/fedora-coreos-34.20210626.3.0-qemu.x86_64.qcow2.xz"
   format = "qcow2"
 
-  depends_on = [null_resource.wait_for_pool]
+  depends_on = [null_resource.wait_for_pool_initialization]
 }
 
 # Define Ignition configs for bootstrap, master, and worker nodes
