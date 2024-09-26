df9ba4cb8cbc965bfc3eddd52bf6a70c39d3fff3
Victor Galvez <vhgalvez@gmail.com>
2024-09-27 00:53:50 +0200

    Update main.tf

--- nat_network_03/main.tf
+++ nat_network_03/main.tf
@@ -18,118 +18,115 @@ provider "libvirt" {
 
 provider "local" {}
 
-# Define and start the storage pool (Ensure the path exists)
+# Define storage pool
 resource "libvirt_pool" "volume_pool" {
   name = "volumetmp_03"
   type = "dir"
   path = "/mnt/lv_data/organized_storage/volumes/volumetmp_03"
 }
 
-# Download Fedora CoreOS base image
+# Base volume for Fedora CoreOS
 resource "libvirt_volume" "fcos_base" {
-  name   = "fcos_base"
+  name   = "fcos_base.qcow2"
   pool   = libvirt_pool.volume_pool.name
-  source = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/34.20210626.3.0/x86_64/fedora-coreos-34.20210626.3.0-qemu.x86_64.qcow2.xz"
+  source = var.coreos_image
   format = "qcow2"
-
   depends_on = [libvirt_pool.volume_pool]
 }
 
+# Create volumes for bootstrap and control planes
+resource "libvirt_volume" "okd_bootstrap" {
+  name           = "okd_bootstrap.qcow2"
+  pool           = libvirt_pool.volume_pool.name
+  size           = var.bootstrap_volume_size * 1073741824
+  base_volume_id = libvirt_volume.fcos_base.id
+}
+
+resource "libvirt_volume" "okd_controlplane_1" {
+  name           = "okd_controlplane_1.qcow2"
+  pool           = libvirt_pool.volume_pool.name
+  size           = var.controlplane_1_volume_size * 1073741824
+  base_volume_id = libvirt_volume.fcos_base.id
+}
+
+resource "libvirt_volume" "okd_controlplane_2" {
+  name           = "okd_controlplane_2.qcow2"
+  pool           = libvirt_pool.volume_pool.name
+  size           = var.controlplane_2_volume_size * 1073741824
+  base_volume_id = libvirt_volume.fcos_base.id
+}
+
+resource "libvirt_volume" "okd_controlplane_3" {
+  name           = "okd_controlplane_3.qcow2"
+  pool           = libvirt_pool.volume_pool.name
+  size           = var.controlplane_3_volume_size * 1073741824
+  base_volume_id = libvirt_volume.fcos_base.id
+}
+
 # Download Ignition files directly from the web server
-data "http" "bootstrap_ignition" {
-  url = "http://10.17.3.14/okd/bootstrap.ign"
+data "http" "ignitions" {
+  urls = [
+    "http://10.17.3.14/okd/bootstrap.ign",
+    "http://10.17.3.14/okd/master.ign",
+    "http://10.17.3.14/okd/worker.ign"
+  ]
 }
 
-data "http" "master_ignition" {
-  url = "http://10.17.3.14/okd/master.ign"
-}
-
-data "http" "worker_ignition" {
-  url = "http://10.17.3.14/okd/worker.ign"
+resource "local_file" "ignition_files" {
+  for_each = toset(["bootstrap", "master", "worker"])
+  content  = data.http.ignitions.response_bodies[each.key]
+  filename = "/tmp/${each.key}.ign"
 }
 
 # Create Ignition volumes from downloaded files
-resource "libvirt_volume" "bootstrap_ign" {
-  name   = "bootstrap-ignition"
-  pool   = libvirt_pool.volume_pool.name
-  source = data.http.bootstrap_ignition.response_body
-  format = "raw"
+resource "libvirt_volume" "ignition_volumes" {
+  for_each = toset(["bootstrap", "master", "worker"])
+  name     = "${each.key}-ignition"
+  pool     = libvirt_pool.volume_pool.name
+  source   = local_file.ignition_files[each.key].filename
+  format   = "raw"
 }
 
-resource "libvirt_volume" "master_ign" {
-  count  = 3
-  name   = "master-${count.index + 1}-ignition"
-  pool   = libvirt_pool.volume_pool.name
-  source = data.http.master_ignition.response_body
-  format = "raw"
-}
+# Define node domains
+resource "libvirt_domain" "nodes" {
+  for_each = {
+    bootstrap = { memory = 8192, vcpu = 4, volume_id = libvirt_volume.okd_bootstrap.id }
+    master1   = { memory = 16384, vcpu = 4, volume_id = libvirt_volume.okd_controlplane_1.id }
+    master2   = { memory = 16384, vcpu = 4, volume_id = libvirt_volume.okd_controlplane_2.id }
+    master3   = { memory = 16384, vcpu = 4, volume_id = libvirt_volume.okd_controlplane_3.id }
+    worker1   = { memory = 8192, vcpu = 4, volume_id = libvirt_volume.okd_controlplane_1.id }
+    worker2   = { memory = 8192, vcpu = 4, volume_id = libvirt_volume.okd_controlplane_2.id }
+    worker3   = { memory = 8192, vcpu = 4, volume_id = libvirt_volume.okd_controlplane_3.id }
+  }
 
-resource "libvirt_volume" "worker_ign" {
-  count  = 3
-  name   = "worker-${count.index + 1}-ignition"
-  pool   = libvirt_pool.volume_pool.name
-  source = data.http.worker_ignition.response_body
-  format = "raw"
-}
+  name   = each.key
+  memory = each.value.memory
+  vcpu   = each.value.vcpu
 
-# Define the bootstrap node
-resource "libvirt_domain" "bootstrap" {
-  name   = "bootstrap"
-  memory = "8192"
-  vcpu   = 4
-
-  cloudinit = libvirt_volume.bootstrap_ign.id
+  cloudinit = libvirt_volume.ignition_volumes["${each.key == "bootstrap" ? "bootstrap" : "master"}"].id
 
   network_interface {
     network_name = "nat_network_02"
   }
 
   disk {
-    volume_id = libvirt_volume.fcos_base.id
-  }
-}
-
-# Define master nodes
-resource "libvirt_domain" "master" {
-  count  = 3
-  name   = "master-${count.index + 1}"
-  memory = "16384"
-  vcpu   = 4
-
-  cloudinit = libvirt_volume.master_ign[count.index].id
-
-  network_interface {
-    network_name = "nat_network_02"
-  }
-
-  disk {
-    volume_id = libvirt_volume.fcos_base.id
-  }
-}
-
-# Define worker nodes
-resource "libvirt_domain" "worker" {
-  count  = 3
-  name   = "worker-${count.index + 1}"
-  memory = "8192"
-  vcpu   = 4
-
-  cloudinit = libvirt_volume.worker_ign[count.index].id
-
-  network_interface {
-    network_name = "nat_network_02"
-  }
-
-  disk {
-    volume_id = libvirt_volume.fcos_base.id
+    volume_id = each.value.volume_id
   }
 }
 
 # Output node IP addresses
 output "ip_addresses" {
   value = {
-    bootstrap = libvirt_domain.bootstrap.network_interface[0].addresses[0]
-    master    = libvirt_domain.master[*].network_interface[0].addresses[0]
-    worker    = libvirt_domain.worker[*].network_interface[0].addresses[0]
+    bootstrap = libvirt_domain.nodes["bootstrap"].network_interface[0].addresses[0]
+    masters   = [
+      libvirt_domain.nodes["master1"].network_interface[0].addresses[0],
+      libvirt_domain.nodes["master2"].network_interface[0].addresses[0],
+      libvirt_domain.nodes["master3"].network_interface[0].addresses[0]
+    ]
+    workers   = [
+      libvirt_domain.nodes["worker1"].network_interface[0].addresses[0],
+      libvirt_domain.nodes["worker2"].network_interface[0].addresses[0],
+      libvirt_domain.nodes["worker3"].network_interface[0].addresses[0]
+    ]
   }
 }
