0dc668ddbc27384ee15d1f6d9e279c6695afdd57
Victor Galvez <vhgalvez@gmail.com>
2024-09-28 12:51:15 +0200

    Update main.tf

--- nat_network_03/main.tf
+++ nat_network_03/main.tf
@@ -1,18 +1,9 @@
 terraform {
   required_version = ">= 1.9.5"
-
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
       version = "0.8.0"
-    }
-    ct = {
-      source  = "poseidon/ct"
-      version = "0.13.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
     }
     local = {
       source  = "hashicorp/local"
@@ -25,7 +16,7 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# Define storage pool
+# Define a storage pool
 resource "libvirt_pool" "volumetmp_03" {
   name = "volumetmp_03"
   type = "dir"
@@ -36,64 +27,59 @@ resource "libvirt_pool" "volumetmp_03" {
   }
 }
 
-# Define node configurations with URLs to the Ignition files
-locals {
-  nodes = {
-    bootstrap = { size = var.bootstrap_volume_size, file = "http://10.17.3.14/okd/bootstrap.ign" },
-    master1   = { size = var.master_volume_size, file = "http://10.17.3.14/okd/master.ign" },
-    master2   = { size = var.master_volume_size, file = "http://10.17.3.14/okd/master.ign" },
-    master3   = { size = var.master_volume_size, file = "http://10.17.3.14/okd/master.ign" },
-    worker1   = { size = var.worker_volume_size, file = "http://10.17.3.14/okd/worker.ign" },
-    worker2   = { size = var.worker_volume_size, file = "http://10.17.3.14/okd/worker.ign" },
-    worker3   = { size = var.worker_volume_size, file = "http://10.17.3.14/okd/worker.ign" }
-  }
-}
-
 # Download Ignition files locally
 data "http" "ignition_files" {
-  for_each = local.nodes
-  url      = each.value.file
+  for_each = {
+    "bootstrap" = "http://10.17.3.14/okd/bootstrap.ign"
+    "master1"   = "http://10.17.3.14/okd/master.ign"
+    "master2"   = "http://10.17.3.14/okd/master.ign"
+    "master3"   = "http://10.17.3.14/okd/master.ign"
+    "worker1"   = "http://10.17.3.14/okd/worker.ign"
+    "worker2"   = "http://10.17.3.14/okd/worker.ign"
+    "worker3"   = "http://10.17.3.14/okd/worker.ign"
+  }
+  url = each.value
 }
 
-resource "local_file" "ignition_local" {
-  for_each = local.nodes
-  content  = data.http.ignition_files[each.key].body
+# Save Ignition files locally
+resource "local_file" "ignition_files" {
+  for_each = data.http.ignition_files
+  content  = each.value.body
   filename = "/mnt/lv_data/organized_storage/volumes/volumetmp_03/${each.key}.ign"
 }
 
 # Create Ignition volumes for nodes
 resource "libvirt_volume" "ignition_volumes" {
-  for_each = local.nodes
+  for_each = local_file.ignition_files
   name     = "${each.key}-ignition"
   pool     = libvirt_pool.volumetmp_03.name
-  # Refers to the Ignition file downloaded locally
-  source   = local_file.ignition_local[each.key].filename
+  source   = each.value.filename
   format   = "raw"
 }
 
-# Create base volume for Fedora CoreOS
+# Download Fedora CoreOS base image and create base volume
 resource "libvirt_volume" "fcos_base" {
   name   = "fcos_base.qcow2"
   pool   = libvirt_pool.volumetmp_03.name
-  source = var.coreos_image
+  source = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/latest.x86_64/fedora-coreos-qemu.x86_64.qcow2.xz"
   format = "qcow2"
 }
 
-# Create node volumes based on the base image
+# Create individual node volumes based on the base image
 resource "libvirt_volume" "okd_volumes" {
-  for_each       = local.nodes
-  name           = "${each.key}.qcow2"
-  pool           = libvirt_pool.volumetmp_03.name
-  size           = each.value.size * 1073741824
+  for_each = data.http.ignition_files
+  name     = "${each.key}.qcow2"
+  pool     = libvirt_pool.volumetmp_03.name
+  size     = var.volume_sizes[each.key] * 1073741824
   base_volume_id = libvirt_volume.fcos_base.id
 }
 
 # Define VMs with network and disk attachments
 resource "libvirt_domain" "nodes" {
-  for_each = local.nodes
-  name     = each.key
-  memory   = var.vm_definitions[each.key].memory
-  vcpu     = var.vm_definitions[each.key].cpus
+  for_each = data.http.ignition_files
+  name     = "okd-${each.key}"
+  memory   = var.vm_memory[each.key]
+  vcpu     = var.vm_cpus[each.key]
 
   cloudinit = libvirt_volume.ignition_volumes[each.key].id
 
