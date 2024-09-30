26392ea9f9c47a3fd93150dbce6555681595b0d1
Victor Galvez <vhgalvez@gmail.com>
2024-09-30 22:25:56 +0200

    Update main.tf

--- br0_network/main.tf
+++ br0_network/main.tf
@@ -92,13 +92,10 @@ resource "libvirt_domain" "vm" {
   }
 
   cloudinit = libvirt_cloudinit_disk.vm_cloudinit[each.key].id
-
   graphics {
-  type        = "spice"
-  listen_type = "address"
-}
-
-
+    type        = "vnc"
+    listen_type = "address"
+  }
 
   console {
     type        = "pty"
