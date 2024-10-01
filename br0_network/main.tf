8a083e2a30084b69d484748c8f747d2fbae912b9
Victor Galvez <vhgalvez@gmail.com>
2024-10-01 17:35:53 +0200

    update

--- br0_network/main.tf
+++ br0_network/main.tf
@@ -1,4 +1,4 @@
-# nat_network_02\main.tf
+# br0_network\main.tf
 terraform {
   required_version = "= 1.9.6"
 
@@ -7,10 +7,6 @@ terraform {
       source  = "dmacvicar/libvirt"
       version = "0.8.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
-    }
   }
 }
 
@@ -18,39 +14,40 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-resource "libvirt_network" "kube_network_02" {
-  name      = "kube_network_02"
-  mode      = "nat"
+resource "libvirt_network" "br0" {
+  name      = var.rocky9_network_name
+  mode      = "bridge"
+  bridge    = "br0"
   autostart = true
-  addresses = ["10.17.3.0/24"]
+  addresses = ["192.168.0.0/24"]
 }
 
-resource "libvirt_pool" "volumetmp_nat_02" {
-  name = "${var.cluster_name}_nat_02"
+resource "libvirt_pool" "volumetmp_bastion" {
+  name = "${var.cluster_name}_bastion"
   type = "dir"
-  path = "/mnt/lv_data/organized_storage/volumes/${var.cluster_name}_nat_02"
+  path = "/mnt/lv_data/organized_storage/volumes/${var.cluster_name}_bastion"
 }
 
 resource "libvirt_volume" "rocky9_image" {
-  name   = "${var.cluster_name}_rocky9_image"
+  name   = "${var.cluster_name}-rocky9_image"
   source = var.rocky9_image
-  pool   = libvirt_pool.volumetmp_nat_02.name
+  pool   = libvirt_pool.volumetmp_bastion.name
   format = "qcow2"
 }
 
-data "template_file" "vm-configs" {
+data "template_file" "vm_configs" {
   for_each = var.vm_rockylinux_definitions
 
   template = file("${path.module}/config/${each.key}-user-data.tpl")
   vars = {
-    ssh_keys = jsonencode(var.ssh_keys),
-    hostname = each.value.hostname,
+    ssh_keys       = jsonencode(var.ssh_keys),
+    hostname       = each.value.hostname,
     short_hostname = each.value.short_hostname,
-    timezone = var.timezone,
-    ip       = each.value.ip,
-    gateway  = var.gateway,
-    dns1     = var.dns1,
-    dns2     = var.dns2
+    timezone       = var.timezone,
+    ip             = each.value.ip,
+    gateway        = each.value.gateway,
+    dns1           = each.value.dns1,
+    dns2           = each.value.dns2
   }
 }
 
@@ -58,51 +55,67 @@ resource "libvirt_cloudinit_disk" "vm_cloudinit" {
   for_each = var.vm_rockylinux_definitions
 
   name      = "${each.key}_cloudinit.iso"
-  pool      = libvirt_pool.volumetmp_nat_02.name
-  user_data = data.template_file.vm-configs[each.key].rendered
+  pool      = libvirt_pool.volumetmp_bastion.name
+  user_data = data.template_file.vm_configs[each.key].rendered
+  network_config = templatefile("${path.module}/config/network-config.tpl", {
+    ip      = each.value.ip,
+    gateway = each.value.gateway,
+    dns1    = each.value.dns1,
+    dns2    = each.value.dns2
+  })
 }
 
 resource "libvirt_volume" "vm_disk" {
   for_each = var.vm_rockylinux_definitions
 
-  name           = "${each.key}-${var.cluster_name}.qcow2"
+  name           = each.value.volume_name
   base_volume_id = libvirt_volume.rocky9_image.id
-  pool           = libvirt_pool.volumetmp_nat_02.name
-  format         = "qcow2"
+  pool           = each.value.volume_pool
+  format         = each.value.volume_format
+  size           = each.value.volume_size
 }
 
-resource "libvirt_domain" "vm_nat_02" {
+resource "libvirt_domain" "vm" {
   for_each = var.vm_rockylinux_definitions
 
-  name   = each.key
-  memory = each.value.domain_memory
+  name   = each.value.hostname
+  memory = each.value.memory
   vcpu   = each.value.cpus
 
   network_interface {
-    network_id     = libvirt_network.kube_network_02.id
-    wait_for_lease = true
-    addresses      = [each.value.ip]
+    network_id = libvirt_network.br0.id
+    bridge     = "br0"
+    addresses  = [each.value.ip] # Assign the static IP
   }
 
   disk {
     volume_id = libvirt_volume.vm_disk[each.key].id
   }
 
+  cloudinit = libvirt_cloudinit_disk.vm_cloudinit[each.key].id
+
   graphics {
     type        = "vnc"
     listen_type = "address"
   }
 
-  cloudinit = libvirt_cloudinit_disk.vm_cloudinit[each.key].id
-
-  cpu {
-    mode = "host-passthrough"
-  }
-
-  # Add this section for serial console support
   console {
     type        = "pty"
     target_type = "serial"
     target_port = "0"
   }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+}
+
+output "bastion_ip_address" {
+  value = var.vm_rockylinux_definitions["bastion1"].ip
 }
