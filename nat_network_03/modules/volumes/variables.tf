8ea22e98640ab441f058aefc60730f98e8c67639
Victor Galvez <vhgalvez@gmail.com>
2024-10-10 00:37:44 +0200

    Update variables.tf

--- nat_network_03/modules/volumes/variables.tf
+++ nat_network_03/modules/volumes/variables.tf
@@ -1,44 +1,12 @@
 # modules/volumes/variables.tf
 
+# Declare CoreOS image variable
 variable "coreos_image" {
   description = "Path to the CoreOS image"
   type        = string
 }
 
-variable "bootstrap_volume_size" {
-  description = "Size of the bootstrap node volume in GiB"
-  type        = number
-}
-
-variable "controlplane_1_volume_size" {
-  description = "Size of the control plane node 1 volume in GiB"
-  type        = number
-}
-
-variable "controlplane_2_volume_size" {
-  description = "Size of the control plane node 2 volume in GiB"
-  type        = number
-}
-
-variable "controlplane_3_volume_size" {
-  description = "Size of the control plane node 3 volume in GiB"
-  type        = number
-}
-
-variable "worker_1_volume_size" {
-  description = "Size of the worker 1 node volume in GiB"
-  type        = number
-}
-
-variable "worker_2_volume_size" {
-  description = "Size of the worker 2 node volume in GiB"
-  type        = number
-}
-
-variable "worker_3_volume_size" {
-  description = "Size of the worker 3 node volume in GiB"
-  type        = number
-}
+# Declare volume size variables for each node type
 
 variable "bootstrap_volume_size" {
   description = "Size of the bootstrap node volume in GiB"
@@ -61,16 +29,16 @@ variable "controlplane_3_volume_size" {
 }
 
 variable "worker_1_volume_size" {
-  description = "Size of the worker 1 node volume in GiB"
+  description = "Size of the worker node 1 volume in GiB"
   type        = number
 }
 
 variable "worker_2_volume_size" {
-  description = "Size of the worker 2 node volume in GiB"
+  description = "Size of the worker node 2 volume in GiB"
   type        = number
 }
 
 variable "worker_3_volume_size" {
-  description = "Size of the worker 3 node volume in GiB"
+  description = "Size of the worker node 3 volume in GiB"
   type        = number
 }
