676e29e739c844702bd13a59747456260276289a
Victor Galvez <vhgalvez@gmail.com>
2024-10-09 22:22:42 +0200

    Update variables.tf

--- nat_network_03/modules/domain/variables.tf
+++ nat_network_03/modules/domain/variables.tf
@@ -1,34 +1,19 @@
-variable "bootstrap_volume" {
-  description = "Volume ID for the bootstrap node"
+variable "network_name" {
+  description = "Name of the network"
   type        = string
 }
 
-variable "controlplane_1_volume" {
-  description = "Volume ID for Control Plane 1"
+variable "bootstrap_ignition" {
+  description = "Ignition config for the bootstrap node"
   type        = string
 }
 
-variable "controlplane_2_volume" {
-  description = "Volume ID for Control Plane 2"
+variable "master_ignition" {
+  description = "Ignition config for the master nodes"
   type        = string
 }
 
-variable "controlplane_3_volume" {
-  description = "Volume ID for Control Plane 3"
-  type        = string
-}
-
-variable "worker_1_volume" {
-  description = "Volume ID for Worker 1"
-  type        = string
-}
-
-variable "worker_2_volume" {
-  description = "Volume ID for Worker 2"
-  type        = string
-}
-
-variable "worker_3_volume" {
-  description = "Volume ID for Worker 3"
+variable "worker_ignition" {
+  description = "Ignition config for the worker nodes"
   type        = string
 }
