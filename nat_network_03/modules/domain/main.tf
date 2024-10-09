e6a0d6c7aeb357669c595d3494553e721b195eea
Victor Galvez <vhgalvez@gmail.com>
2024-10-10 01:38:59 +0200

    Update main.tf

--- nat_network_03/modules/domain/main.tf
+++ nat_network_03/modules/domain/main.tf
@@ -1,4 +1,5 @@
 # modules/domain/main.tf
+
 terraform {
   required_version = ">= 1.9.6"
 
@@ -13,14 +14,13 @@ terraform {
 # Bootstrap Node Definition
 resource "libvirt_domain" "okd_bootstrap" {
   name            = var.bootstrap.name
-  description     = var.bootstrap.description
   vcpu            = var.bootstrap.vcpu
-  memory          = var.bootstrap.memory * 1024  # Convert MiB to bytes
+  memory          = var.bootstrap.memory * 1024 # Convert memory from MiB
   running         = true
-  coreos_ignition = var.bootstrap_ignition_id  # Use Ignition ID
+  coreos_ignition = var.bootstrap_ignition_id
 
   disk {
-    volume_id = var.bootstrap_volume_id  # Use Volume ID
+    volume_id = var.bootstrap_volume_id
     scsi      = false
   }
 
@@ -40,7 +40,7 @@ resource "libvirt_domain" "okd_bootstrap" {
   }
 
   network_interface {
-    network_id     = var.network_id  # Use network ID
+    network_id     = var.network_id
     hostname       = var.bootstrap.name
     addresses      = [var.bootstrap.address]
     mac            = var.bootstrap.mac
@@ -51,14 +51,13 @@ resource "libvirt_domain" "okd_bootstrap" {
 # Control Plane 1 Node Definition
 resource "libvirt_domain" "okd_controlplane_1" {
   name            = var.controlplane_1.name
-  description     = var.controlplane_1.description
   vcpu            = var.controlplane_1.vcpu
-  memory          = var.controlplane_1.memory * 1024  # MiB to bytes
+  memory          = var.controlplane_1.memory * 1024
   running         = true
-  coreos_ignition = var.master_ignition_id  # Master Ignition ID
+  coreos_ignition = var.master_ignition_id
 
   disk {
-    volume_id = var.controlplane_1_volume_id  # Use Volume ID
+    volume_id = var.controlplane_1_volume_id
     scsi      = false
   }
 
@@ -78,7 +77,7 @@ resource "libvirt_domain" "okd_controlplane_1" {
   }
 
   network_interface {
-    network_id     = var.network_id  # Use network ID
+    network_id     = var.network_id
     hostname       = var.controlplane_1.name
     addresses      = [var.controlplane_1.address]
     mac            = var.controlplane_1.mac
@@ -89,14 +88,13 @@ resource "libvirt_domain" "okd_controlplane_1" {
 # Control Plane 2 Node Definition
 resource "libvirt_domain" "okd_controlplane_2" {
   name            = var.controlplane_2.name
-  description     = var.controlplane_2.description
   vcpu            = var.controlplane_2.vcpu
-  memory          = var.controlplane_2.memory * 1024  # MiB to bytes
+  memory          = var.controlplane_2.memory * 1024
   running         = true
-  coreos_ignition = var.master_ignition_id  # Master Ignition ID
+  coreos_ignition = var.master_ignition_id
 
   disk {
-    volume_id = var.controlplane_2_volume_id  # Use Volume ID
+    volume_id = var.controlplane_2_volume_id
     scsi      = false
   }
 
@@ -116,7 +114,7 @@ resource "libvirt_domain" "okd_controlplane_2" {
   }
 
   network_interface {
-    network_id     = var.network_id  # Use network ID
+    network_id     = var.network_id
     hostname       = var.controlplane_2.name
     addresses      = [var.controlplane_2.address]
     mac            = var.controlplane_2.mac
@@ -127,14 +125,13 @@ resource "libvirt_domain" "okd_controlplane_2" {
 # Control Plane 3 Node Definition
 resource "libvirt_domain" "okd_controlplane_3" {
   name            = var.controlplane_3.name
-  description     = var.controlplane_3.description
   vcpu            = var.controlplane_3.vcpu
-  memory          = var.controlplane_3.memory * 1024  # MiB to bytes
+  memory          = var.controlplane_3.memory * 1024
   running         = true
-  coreos_ignition = var.master_ignition_id  # Master Ignition ID
+  coreos_ignition = var.master_ignition_id
 
   disk {
-    volume_id = var.controlplane_3_volume_id  # Use Volume ID
+    volume_id = var.controlplane_3_volume_id
     scsi      = false
   }
 
@@ -154,7 +151,7 @@ resource "libvirt_domain" "okd_controlplane_3" {
   }
 
   network_interface {
-    network_id     = var.network_id  # Use network ID
+    network_id     = var.network_id
     hostname       = var.controlplane_3.name
     addresses      = [var.controlplane_3.address]
     mac            = var.controlplane_3.mac
@@ -162,17 +159,16 @@ resource "libvirt_domain" "okd_controlplane_3" {
   }
 }
 
-# Worker Node Definitions (1, 2, 3 follow the same pattern)
+# Worker 1 Node Definition
 resource "libvirt_domain" "okd_worker_1" {
   name            = var.worker_1.name
-  description     = var.worker_1.description
   vcpu            = var.worker_1.vcpu
-  memory          = var.worker_1.memory * 1024  # MiB to bytes
+  memory          = var.worker_1.memory * 1024
   running         = true
-  coreos_ignition = var.worker_ignition_id  # Use worker Ignition ID
+  coreos_ignition = var.worker_ignition_id
 
   disk {
-    volume_id = var.worker_1_volume_id  # Use Volume ID
+    volume_id = var.worker_1_volume_id
     scsi      = false
   }
 
@@ -192,7 +188,7 @@ resource "libvirt_domain" "okd_worker_1" {
   }
 
   network_interface {
-    network_id     = var.network_id  # Use network ID
+    network_id     = var.network_id
     hostname       = var.worker_1.name
     addresses      = [var.worker_1.address]
     mac            = var.worker_1.mac
@@ -200,16 +196,16 @@ resource "libvirt_domain" "okd_worker_1" {
   }
 }
 
+# Worker 2 Node Definition
 resource "libvirt_domain" "okd_worker_2" {
   name            = var.worker_2.name
-  description     = var.worker_2.description
   vcpu            = var.worker_2.vcpu
-  memory          = var.worker_2.memory * 1024  # MiB to bytes
+  memory          = var.worker_2.memory * 1024
   running         = true
-  coreos_ignition = var.worker_ignition_id  # Use worker Ignition ID
+  coreos_ignition = var.worker_ignition_id
 
   disk {
-    volume_id = var.worker_2_volume_id  # Use Volume ID
+    volume_id = var.worker_2_volume_id
     scsi      = false
   }
 
@@ -229,7 +225,7 @@ resource "libvirt_domain" "okd_worker_2" {
   }
 
   network_interface {
-    network_id     = var.network_id  # Use network ID
+    network_id     = var.network_id
     hostname       = var.worker_2.name
     addresses      = [var.worker_2.address]
     mac            = var.worker_2.mac
@@ -237,16 +233,16 @@ resource "libvirt_domain" "okd_worker_2" {
   }
 }
 
+# Worker 3 Node Definition
 resource "libvirt_domain" "okd_worker_3" {
   name            = var.worker_3.name
-  description     = var.worker_3.description
   vcpu            = var.worker_3.vcpu
-  memory          = var.worker_3.memory * 1024  # MiB to bytes
+  memory          = var.worker_3.memory * 1024
   running         = true
-  coreos_ignition = var.worker_ignition_id  # Use worker Ignition ID
+  coreos_ignition = var.worker_ignition_id
 
   disk {
-    volume_id = var.worker_3_volume_id  # Use Volume ID
+    volume_id = var.worker_3_volume_id
     scsi      = false
   }
 
@@ -266,7 +262,7 @@ resource "libvirt_domain" "okd_worker_3" {
   }
 
   network_interface {
-    network_id     = var.network_id  # Use network ID
+    network_id     = var.network_id
     hostname       = var.worker_3.name
     addresses      = [var.worker_3.address]
     mac            = var.worker_3.mac
