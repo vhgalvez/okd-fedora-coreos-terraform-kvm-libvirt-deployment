b231a8d7a61cf924c877987454168d40f4af9d3a
Victor Galvez <vhgalvez@gmail.com>
2024-10-09 20:28:59 +0200

    Update main.tf

--- nat_network_03/modules/domain/main.tf
+++ nat_network_03/modules/domain/main.tf
@@ -1,5 +1,3 @@
-# modules/domain/main.tf
-
 terraform {
   required_version = ">= 1.9.6"
 
@@ -15,10 +13,10 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-
+# Bootstrap Node Definition
 resource "libvirt_domain" "okd_bootstrap" {
   name            = var.bootstrap.name
-  memory          = var.bootstrap.memory * 1024
+  memory          = var.bootstrap.memory * 1024 # Convert memory from MiB
   vcpu            = var.bootstrap.vcpu
   running         = true
   coreos_ignition = var.bootstrap_ignition
@@ -33,6 +31,229 @@ resource "libvirt_domain" "okd_bootstrap" {
     addresses    = [var.bootstrap.address]
     mac          = var.bootstrap.mac
   }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
 }
 
-# Define similar resources for master and worker nodes...
+# Control Plane 1 Node Definition
+resource "libvirt_domain" "okd_controlplane_1" {
+  name            = var.controlplane_1.name
+  memory          = var.controlplane_1.memory * 1024
+  vcpu            = var.controlplane_1.vcpu
+  running         = true
+  coreos_ignition = var.master_ignition
+
+  disk {
+    volume_id = var.controlplane_1_volume
+  }
+
+  network_interface {
+    network_name = var.network_name
+    hostname     = var.controlplane_1.name
+    addresses    = [var.controlplane_1.address]
+    mac          = var.controlplane_1.mac
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+}
+
+# Control Plane 2 Node Definition
+resource "libvirt_domain" "okd_controlplane_2" {
+  name            = var.controlplane_2.name
+  memory          = var.controlplane_2.memory * 1024
+  vcpu            = var.controlplane_2.vcpu
+  running         = true
+  coreos_ignition = var.master_ignition
+
+  disk {
+    volume_id = var.controlplane_2_volume
+  }
+
+  network_interface {
+    network_name = var.network_name
+    hostname     = var.controlplane_2.name
+    addresses    = [var.controlplane_2.address]
+    mac          = var.controlplane_2.mac
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+}
+
+# Control Plane 3 Node Definition
+resource "libvirt_domain" "okd_controlplane_3" {
+  name            = var.controlplane_3.name
+  memory          = var.controlplane_3.memory * 1024
+  vcpu            = var.controlplane_3.vcpu
+  running         = true
+  coreos_ignition = var.master_ignition
+
+  disk {
+    volume_id = var.controlplane_3_volume
+  }
+
+  network_interface {
+    network_name = var.network_name
+    hostname     = var.controlplane_3.name
+    addresses    = [var.controlplane_3.address]
+    mac          = var.controlplane_3.mac
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+}
+
+# Worker 1 Node Definition
+resource "libvirt_domain" "okd_worker_1" {
+  name            = var.worker_1.name
+  memory          = var.worker_1.memory * 1024
+  vcpu            = var.worker_1.vcpu
+  running         = true
+  coreos_ignition = var.worker_ignition
+
+  disk {
+    volume_id = var.worker_1_volume
+  }
+
+  network_interface {
+    network_name = var.network_name
+    hostname     = var.worker_1.name
+    addresses    = [var.worker_1.address]
+    mac          = var.worker_1.mac
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+}
+
+# Worker 2 Node Definition
+resource "libvirt_domain" "okd_worker_2" {
+  name            = var.worker_2.name
+  memory          = var.worker_2.memory * 1024
+  vcpu            = var.worker_2.vcpu
+  running         = true
+  coreos_ignition = var.worker_ignition
+
+  disk {
+    volume_id = var.worker_2_volume
+  }
+
+  network_interface {
+    network_name = var.network_name
+    hostname     = var.worker_2.name
+    addresses    = [var.worker_2.address]
+    mac          = var.worker_2.mac
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+}
+
+# Worker 3 Node Definition
+resource "libvirt_domain" "okd_worker_3" {
+  name            = var.worker_3.name
+  memory          = var.worker_3.memory * 1024
+  vcpu            = var.worker_3.vcpu
+  running         = true
+  coreos_ignition = var.worker_ignition
+
+  disk {
+    volume_id = var.worker_3_volume
+  }
+
+  network_interface {
+    network_name = var.network_name
+    hostname     = var.worker_3.name
+    addresses    = [var.worker_3.address]
+    mac          = var.worker_3.mac
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type     = "vnc"
+    autoport = true
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+}
