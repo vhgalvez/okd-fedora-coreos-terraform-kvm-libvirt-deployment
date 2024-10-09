6d1f861104287c138d30086c1515d103043a60df
Victor Galvez <vhgalvez@gmail.com>
2024-10-10 00:23:59 +0200

    Update variables.tf

--- nat_network_03/modules/volumes/variables.tf
+++ nat_network_03/modules/volumes/variables.tf
@@ -20,19 +20,3 @@ variable "controlplane_3_volume_size" {
   description = "Size of the control plane node 3 volume in GiB"
   type        = number
 }
-
-# Declare worker volume sizes
-variable "worker_1_volume_size" {
-  description = "Size of worker node 1 volume in GiB"
-  type        = number
-}
-
-variable "worker_2_volume_size" {
-  description = "Size of worker node 2 volume in GiB"
-  type        = number
-}
-
-variable "worker_3_volume_size" {
-  description = "Size of worker node 3 volume in GiB"
-  type        = number
-}
