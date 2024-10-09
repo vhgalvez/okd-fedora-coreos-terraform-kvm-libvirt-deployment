a1d07fd8f4e2070b8c408ac2de6a0b2c1340e866
Victor Galvez <vhgalvez@gmail.com>
2024-10-10 00:35:08 +0200

    Update variables.tf

--- nat_network_03/modules/volumes/variables.tf
+++ nat_network_03/modules/volumes/variables.tf
@@ -1,7 +1,11 @@
 # modules/volumes/variables.tf
 
- 
- variable "bootstrap_volume_size" {
+ variable "coreos_image" {
+  description = "Path to the CoreOS image"
+  type        = string
+}
+
+variable "bootstrap_volume_size" {
   description = "Size of the bootstrap node volume in GiB"
   type        = number
 }
@@ -18,5 +22,55 @@ variable "controlplane_2_volume_size" {
 
 variable "controlplane_3_volume_size" {
   description = "Size of the control plane node 3 volume in GiB"
+  type        = number
+}
+
+variable "worker_1_volume_size" {
+  description = "Size of the worker 1 node volume in GiB"
+  type        = number
+}
+
+variable "worker_2_volume_size" {
+  description = "Size of the worker 2 node volume in GiB"
+  type        = number
+}
+
+variable "worker_3_volume_size" {
+  description = "Size of the worker 3 node volume in GiB"
+  type        = number
+}
+
+variable "bootstrap_volume_size" {
+  description = "Size of the bootstrap node volume in GiB"
+  type        = number
+}
+
+variable "controlplane_1_volume_size" {
+  description = "Size of the control plane node 1 volume in GiB"
+  type        = number
+}
+
+variable "controlplane_2_volume_size" {
+  description = "Size of the control plane node 2 volume in GiB"
+  type        = number
+}
+
+variable "controlplane_3_volume_size" {
+  description = "Size of the control plane node 3 volume in GiB"
+  type        = number
+}
+
+variable "worker_1_volume_size" {
+  description = "Size of the worker 1 node volume in GiB"
+  type        = number
+}
+
+variable "worker_2_volume_size" {
+  description = "Size of the worker 2 node volume in GiB"
+  type        = number
+}
+
+variable "worker_3_volume_size" {
+  description = "Size of the worker 3 node volume in GiB"
   type        = number
 }
