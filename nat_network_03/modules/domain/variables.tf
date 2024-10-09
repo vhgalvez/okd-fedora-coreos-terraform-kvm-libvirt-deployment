284a8c919a14b589588fb8e9cf33453d0aa25316
Victor Galvez <vhgalvez@gmail.com>
2024-10-09 22:50:16 +0200

    Update variables.tf

--- nat_network_03/modules/domain/variables.tf
+++ nat_network_03/modules/domain/variables.tf
@@ -1,19 +1,54 @@
+variable "bootstrap_volume" {
+  description = "Volume ID for the bootstrap node"
+  type        = string
+}
+
+variable "controlplane_1_volume" {
+  description = "Volume ID for control plane node 1"
+  type        = string
+}
+
+variable "controlplane_2_volume" {
+  description = "Volume ID for control plane node 2"
+  type        = string
+}
+
+variable "controlplane_3_volume" {
+  description = "Volume ID for control plane node 3"
+  type        = string
+}
+
+variable "worker_1_volume" {
+  description = "Volume ID for worker node 1"
+  type        = string
+}
+
+variable "worker_2_volume" {
+  description = "Volume ID for worker node 2"
+  type        = string
+}
+
+variable "worker_3_volume" {
+  description = "Volume ID for worker node 3"
+  type        = string
+}
+
 variable "network_name" {
-  description = "Name of the network"
+  description = "Name of the libvirt network"
   type        = string
 }
 
 variable "bootstrap_ignition" {
-  description = "Ignition config for the bootstrap node"
+  description = "Ignition ID for bootstrap node"
   type        = string
 }
 
 variable "master_ignition" {
-  description = "Ignition config for the master nodes"
+  description = "Ignition ID for master nodes"
   type        = string
 }
 
 variable "worker_ignition" {
-  description = "Ignition config for the worker nodes"
+  description = "Ignition ID for worker nodes"
   type        = string
 }
