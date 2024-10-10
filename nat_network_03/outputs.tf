525725b89427bea8583fc691aa7f70b0507ae4d8
Victor Galvez <vhgalvez@gmail.com>
2024-10-10 03:52:09 +0200

    Update outputs.tf

--- nat_network_03/outputs.tf
+++ nat_network_03/outputs.tf
@@ -1,34 +1,29 @@
-output "bootstrap_ip" {
-  description = "IP address of the Bootstrap node"
-  value = module.domain.okd_bootstrap.network_interface.0.addresses[0]
+# modules/domain/outputs.tf
+
+output "okd_bootstrap_ip" {
+  value = libvirt_domain.okd_bootstrap.network_interface.0.addresses[0]
 }
 
-output "controlplane_1_ip" {
-  description = "IP address of Control Plane node 1"
-  value = module.domain.okd_controlplane_1.network_interface.0.addresses[0]
+output "okd_controlplane_1_ip" {
+  value = libvirt_domain.okd_controlplane_1.network_interface.0.addresses[0]
 }
 
-output "controlplane_2_ip" {
-  description = "IP address of Control Plane node 2"
-  value = module.domain.okd_controlplane_2.network_interface.0.addresses[0]
+output "okd_controlplane_2_ip" {
+  value = libvirt_domain.okd_controlplane_2.network_interface.0.addresses[0]
 }
 
-output "controlplane_3_ip" {
-  description = "IP address of Control Plane node 3"
-  value = module.domain.okd_controlplane_3.network_interface.0.addresses[0]
+output "okd_controlplane_3_ip" {
+  value = libvirt_domain.okd_controlplane_3.network_interface.0.addresses[0]
 }
 
-output "worker_1_ip" {
-  description = "IP address of Worker node 1"
-  value = module.domain.okd_worker_1.network_interface.0.addresses[0]
+output "okd_worker_1_ip" {
+  value = libvirt_domain.okd_worker_1.network_interface.0.addresses[0]
 }
 
-output "worker_2_ip" {
-  description = "IP address of Worker node 2"
-  value = module.domain.okd_worker_2.network_interface.0.addresses[0]
+output "okd_worker_2_ip" {
+  value = libvirt_domain.okd_worker_2.network_interface.0.addresses[0]
 }
 
-output "worker_3_ip" {
-  description = "IP address of Worker node 3"
-  value = module.domain.okd_worker_3.network_interface.0.addresses[0]
+output "okd_worker_3_ip" {
+  value = libvirt_domain.okd_worker_3.network_interface.0.addresses[0]
 }
