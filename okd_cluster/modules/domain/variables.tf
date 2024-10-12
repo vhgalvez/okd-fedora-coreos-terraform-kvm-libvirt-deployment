d669e420433b0f10a633b474f991f0b636297bd4
Victor Galvez <vhgalvez@gmail.com>
2024-10-12 16:55:42 +0200

    Update variables.tf

--- okd_cluster/modules/domain/variables.tf
+++ okd_cluster/modules/domain/variables.tf
@@ -98,3 +98,7 @@ variable "network_name" {
   description = "Name of the network to which the nodes should be attached"
   type        = string
 }
+variable "network_name" {
+  description = "Nombre de la red para conectar los nodos"
+  type        = string
+}
