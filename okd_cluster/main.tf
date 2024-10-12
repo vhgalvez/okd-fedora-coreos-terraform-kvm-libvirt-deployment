88432cd8eba887ad76412d4b4c878b45672d9bc4
Victor Galvez <vhgalvez@gmail.com>
2024-10-12 16:19:48 +0200

    Update main.tf

--- okd_cluster/main.tf
+++ okd_cluster/main.tf
@@ -40,7 +40,7 @@ module "volumes" {
 
 # Módulo de dominios para crear las VMs para bootstrap, control plane y nodos worker
 module "domain" {
-  source = "./modules/domain"  # Asegúrate de que este directorio exista y esté bien escrito
+  source = "./modules/domain"  # No debería haber una carpeta llamada 'modules' dentro de 'domain'
 
   network_name          = "kube_network_02"  # Cambiado de network_id a network_name
   bootstrap_ignition_id = module.ignition.bootstrap_ignition 
@@ -92,4 +92,3 @@ output "worker_2" {
 output "worker_3" {
   value = module.domain.okd_worker_3
 }
-
