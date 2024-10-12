c6c64ba2f71d7dc80fb74292eeb3414bec754808
Victor Galvez <vhgalvez@gmail.com>
2024-10-12 19:54:36 +0200

    Update outputs.tf

--- okd_cluster/outputs.tf
+++ okd_cluster/outputs.tf
@@ -1,43 +1,29 @@
 # outputs.tf
 
-# Output para la IP del nodo bootstrap
-output "bootstrap_ip" {
-  value       = module.domain.okd_bootstrap
-  description = "La IP del nodo bootstrap"
+output "bootstrap" {
+  value = module.domain.okd_bootstrap
 }
 
-# Output para la IP del nodo master 1
-output "master1_ip" {
-  value       = module.domain.okd_controlplane_1
-  description = "La IP del nodo master 1"
+output "controlplane_1" {
+  value = module.domain.okd_controlplane_1
 }
 
-# Output para la IP del nodo master 2
-output "master2_ip" {
-  value       = module.domain.okd_controlplane_2
-  description = "La IP del nodo master 2"
+output "controlplane_2" {
+  value = module.domain.okd_controlplane_2
 }
 
-# Output para la IP del nodo master 3
-output "master3_ip" {
-  value       = module.domain.okd_controlplane_3
-  description = "La IP del nodo master 3"
+output "controlplane_3" {
+  value = module.domain.okd_controlplane_3
 }
 
-# Output para la IP del nodo worker 1
-output "worker1_ip" {
-  value       = module.domain.okd_worker_1
-  description = "La IP del nodo worker 1"
+output "worker_1" {
+  value = module.domain.okd_worker_1
 }
 
-# Output para la IP del nodo worker 2
-output "worker2_ip" {
-  value       = module.domain.okd_worker_2
-  description = "La IP del nodo worker 2"
+output "worker_2" {
+  value = module.domain.okd_worker_2
 }
 
-# Output para la IP del nodo worker 3
-output "worker3_ip" {
-  value       = module.domain.okd_worker_3
-  description = "La IP del nodo worker 3"
+output "worker_3" {
+  value = module.domain.okd_worker_3
 }
