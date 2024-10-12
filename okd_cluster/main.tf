aec101ad9179d5362971ec9f26e2e6ff23f68ddf
Victor Galvez <vhgalvez@gmail.com>
2024-10-12 18:39:39 +0200

    update

--- okd_cluster/main.tf
+++ okd_cluster/main.tf
@@ -43,13 +43,14 @@ module "domain" {
   source       = "./modules/domain"
   network_name = var.network_name
 
-  # Pasar coreos_image al módulo
   coreos_image = var.coreos_image
 
+  # Ignition IDs
   bootstrap_ignition_id = module.ignition.bootstrap_ignition
   master_ignition_id    = module.ignition.master_ignition
   worker_ignition_id    = module.ignition.worker_ignition
 
+  # Volume IDs
   bootstrap_volume_id      = module.volumes.bootstrap_volume.id
   controlplane_1_volume_id = module.volumes.controlplane_1_volume.id
   controlplane_2_volume_id = module.volumes.controlplane_2_volume.id
@@ -57,7 +58,17 @@ module "domain" {
   worker_1_volume_id       = module.volumes.worker_1_volume.id
   worker_2_volume_id       = module.volumes.worker_2_volume.id
   worker_3_volume_id       = module.volumes.worker_3_volume.id
+
+  # Node definitions
+  bootstrap      = var.bootstrap
+  controlplane_1 = var.controlplane_1
+  controlplane_2 = var.controlplane_2
+  controlplane_3 = var.controlplane_3
+  worker_1       = var.worker_1
+  worker_2       = var.worker_2
+  worker_3       = var.worker_3
 }
+
 
 
 
--- okd_cluster/variables.tf
+++ okd_cluster/variables.tf
@@ -4,6 +4,60 @@ variable "coreos_image" {
   description = "Path to the CoreOS image"
 }
 
+variable "bootstrap_volume_size" {
+  type        = number
+  description = "Size of the bootstrap node volume in GiB"
+}
+
+variable "controlplane_1_volume_size" {
+  type        = number
+  description = "Size of the control plane node 1 volume in GiB"
+}
+
+variable "controlplane_2_volume_size" {
+  type        = number
+  description = "Size of the control plane node 2 volume in GiB"
+}
+
+variable "controlplane_3_volume_size" {
+  type        = number
+  description = "Size of the control plane node 3 volume in GiB"
+}
+
+variable "worker_1_volume_size" {
+  type        = number
+  description = "Size of the worker node 1 volume in GiB"
+}
+
+variable "worker_2_volume_size" {
+  type        = number
+  description = "Size of the worker node 2 volume in GiB"
+}
+
+variable "worker_3_volume_size" {
+  type        = number
+  description = "Size of the worker node 3 volume in GiB"
+}
+
+variable "dns1" {
+  type        = string
+  description = "Primary DNS server"
+}
+
+variable "dns2" {
+  type        = string
+  description = "Secondary DNS server"
+}
+
+variable "gateway" {
+  type        = string
+  description = "Network gateway"
+}
+variable "network_name" {
+  description = "Nombre de la red para conectar los nodos"
+  type        = string
+}
+
 variable "bootstrap" {
   type = object({
     name    = string
@@ -79,58 +133,4 @@ variable "worker_3" {
     mac     = string
   })
   description = "Worker node 3 definition"
-}
-
-variable "bootstrap_volume_size" {
-  type        = number
-  description = "Size of the bootstrap node volume in GiB"
-}
-
-variable "controlplane_1_volume_size" {
-  type        = number
-  description = "Size of the control plane node 1 volume in GiB"
-}
-
-variable "controlplane_2_volume_size" {
-  type        = number
-  description = "Size of the control plane node 2 volume in GiB"
-}
-
-variable "controlplane_3_volume_size" {
-  type        = number
-  description = "Size of the control plane node 3 volume in GiB"
-}
-
-variable "worker_1_volume_size" {
-  type        = number
-  description = "Size of the worker node 1 volume in GiB"
-}
-
-variable "worker_2_volume_size" {
-  type        = number
-  description = "Size of the worker node 2 volume in GiB"
-}
-
-variable "worker_3_volume_size" {
-  type        = number
-  description = "Size of the worker node 3 volume in GiB"
-}
-
-variable "dns1" {
-  type        = string
-  description = "Primary DNS server"
-}
-
-variable "dns2" {
-  type        = string
-  description = "Secondary DNS server"
-}
-
-variable "gateway" {
-  type        = string
-  description = "Network gateway"
-}
-variable "network_name" {
-  description = "Nombre de la red para conectar los nodos"
-  type        = string
 }
