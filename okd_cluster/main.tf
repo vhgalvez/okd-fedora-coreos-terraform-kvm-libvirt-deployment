4a9f912fd198a2b32c5c2d37239deecc4e0d4451
Victor Galvez <vhgalvez@gmail.com>
2024-10-12 18:29:01 +0200

    update

--- okd_cluster/main.tf
+++ okd_cluster/main.tf
@@ -40,27 +40,23 @@ module "volumes" {
 
 # Módulo de dominios
 module "domain" {
-  source                = "./modules/domain"
-  network_name          = module.network.network_name
+  source       = "./modules/domain"
+  network_name = var.network_name
+
+  # Pasar coreos_image al módulo
+  coreos_image = var.coreos_image
+
   bootstrap_ignition_id = module.ignition.bootstrap_ignition
   master_ignition_id    = module.ignition.master_ignition
   worker_ignition_id    = module.ignition.worker_ignition
 
-  bootstrap_volume_id      = module.volumes.bootstrap_volume
-  controlplane_1_volume_id = module.volumes.controlplane_1_volume
-  controlplane_2_volume_id = module.volumes.controlplane_2_volume
-  controlplane_3_volume_id = module.volumes.controlplane_3_volume
-  worker_1_volume_id       = module.volumes.worker_1_volume
-  worker_2_volume_id       = module.volumes.worker_2_volume
-  worker_3_volume_id       = module.volumes.worker_3_volume
-
-  bootstrap      = var.bootstrap
-  controlplane_1 = var.controlplane_1
-  controlplane_2 = var.controlplane_2
-  controlplane_3 = var.controlplane_3
-  worker_1       = var.worker_1
-  worker_2       = var.worker_2
-  worker_3       = var.worker_3
+  bootstrap_volume_id      = module.volumes.bootstrap_volume.id
+  controlplane_1_volume_id = module.volumes.controlplane_1_volume.id
+  controlplane_2_volume_id = module.volumes.controlplane_2_volume.id
+  controlplane_3_volume_id = module.volumes.controlplane_3_volume.id
+  worker_1_volume_id       = module.volumes.worker_1_volume.id
+  worker_2_volume_id       = module.volumes.worker_2_volume.id
+  worker_3_volume_id       = module.volumes.worker_3_volume.id
 }
 
 
